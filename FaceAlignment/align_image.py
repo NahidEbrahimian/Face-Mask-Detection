43c966cf0c5067e82761c59ccdc0db74bc09977b
NahidEbrahimian <nsebrahimian@gmail.com>
2021-10-05 23:48:06 +0330

    update

--- FaceAlignment/align_image.py
+++ FaceAlignment/align_image.py
@@ -2,30 +2,21 @@ import argparse
 from .face_alignment import image_align
 from .landmarks_detector import LandmarksDetector
 
-# if __name__ == "__main__":
 def main(img):
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', help='input image path', default=img, type=str)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--input', help='input image path', default=img, type=str)
     # parser.add_argument('--output', help='output image path', default='output/', type=str)
-    parser.add_argument('--landmarks-model-path', help='landmarks model path', default='FaceAlignment/models/shape_predictor_68_face_landmarks.dat', type=str)
-    args = parser.parse_args()
+    # parser.add_argument('--landmarks-model-path', help='landmarks model path', default='FaceAlignment/models/shape_predictor_68_face_landmarks.dat', type=str)
+    # args = parser.parse_args()
 
-    # file_name, file_ext = os.path.splitext(os.path.basename(args.input))
-    landmarks_detector = LandmarksDetector(args.landmarks_model_path)
+    landmarks_detector = LandmarksDetector('FaceAlignment/models/shape_predictor_68_face_landmarks.dat')
+    all_face_landmarks = landmarks_detector.get_landmarks(img)
+
     try:
-        all_face_landmarks = landmarks_detector.get_landmarks(args.input)
         for i, face_landmarks in enumerate(all_face_landmarks):
-            # image = image_align(args.input, face_landmarks)
-            image, bb = image_align(args.input, face_landmarks)
-            print(image)
-            print(bb)
-            return image, bb
-            # output_file_path = os.path.join(args.output, file_name + "_" + str(i) + ".jpg")
-            # cv2.imwrite(output_file_path, image)
+            image, bb = image_align(img, face_landmarks)
+            yield image, bb
 
-    except Exception as e:
-        print("Error:", e)
-
-if __name__ == "__main__":
-    main()
+    except:
+        pass
--- inference-img.py
+++ inference-img.py
@@ -6,7 +6,6 @@ from keras.models import load_model
 from FaceAlignment.align_image import main
 import argparse
 
-
 def get_optimal_font_scale(text, width):
 
     for scale in reversed(range(0, 60, 1)):
@@ -16,9 +15,8 @@ def get_optimal_font_scale(text, width):
             return scale/10
     return 1
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', help='input image path', default='input/03.jpg', type=str)
+parser.add_argument("--input", default='input/02.jpg', type=str)
 args = parser.parse_args()
 
 model = load_model("weights/model.h5")
@@ -27,25 +25,34 @@ file_name, file_ext = os.path.splitext(os.path.basename(args.input))
 
 img = cv2.imread(args.input)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-face, bb = main(img)
+face_BB = main(img)
 
-face_img = cv2.resize(face, (width, height))
-face_img = face_img / 255.0
-face_img = face_img.reshape(1, width, height, 3)
+try:
+    face, BB = next(face_BB)
+    face_img = cv2.resize(face, (width, height))
+    face_img = face_img / 255.0
+    face_img = face_img.reshape(1, width, height, 3)
 
-y_pred = model.predict(face_img)
-prediction = np.argmax(y_pred)
+    y_pred = model.predict(face_img)
+    prediction = np.argmax(y_pred)
 
-if prediction == 0:
-  y_pred = "With Mask"
-else:
-  y_pred = "Without Mask"
+    if prediction == 0:
+      y_pred = "With Mask"
+    else:
+      y_pred = "Without Mask"
 
-bb = np.array(bb)
-font_size = get_optimal_font_scale(y_pred, img_rgb.shape[1] // 4)
-cv2.rectangle(img, pt1=(bb[0], bb[1]), pt2=(bb[2], bb[3]), color=(0, 255, 0),thickness=2)
-cv2.putText(img, y_pred, (img_rgb.shape[0] // 12, img_rgb.shape[1] // 12), cv2.FONT_HERSHEY_SIMPLEX, font_size, (0, 255, 0), 2,
-                cv2.LINE_AA)
+    BB = np.array(BB)
+    font_size = get_optimal_font_scale(y_pred, img_rgb.shape[1] // 4)
+    cv2.rectangle(img, pt1=(BB[0], BB[1]), pt2=(BB[2], BB[3]), color=(0, 255, 0),thickness=2)
+    cv2.putText(img, y_pred, (img_rgb.shape[0] // 12, img_rgb.shape[1] // 12), cv2.FONT_HERSHEY_SIMPLEX, font_size, (0, 255, 0), 2,
+                    cv2.LINE_AA)
+
+except:
+    y_pred = 'Face not Detected'
+    font_size = get_optimal_font_scale(y_pred, img_rgb.shape[1] // 4)
+    cv2.putText(img, y_pred, (img_rgb.shape[0] // 12, img_rgb.shape[1] // 12), cv2.FONT_HERSHEY_SIMPLEX, font_size, (0, 255, 0), 2,
+                    cv2.LINE_AA)
+
 
 cv2.imshow('Face Mask Detection', img)
 cv2.imwrite(os.path.join('output/{}'.format(file_name)+ '.jpg'), img)
